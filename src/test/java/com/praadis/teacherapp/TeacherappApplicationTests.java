938bf446046dd4eb5c3a1421017ed932870c4a88
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-06-28 22:04:39 +0530

    Basic Spring Boot Project initlization

---
+++ src/test/java/com/praadis/teacherapp/TeacherappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praadis.teacherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
