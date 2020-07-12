e2f9d16dd92f3c9b2accf2ce8052a950617760c4
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-07-13 04:35:31 +0530

    Use the Message Digest Security along with the credentials in sql file #5

--- src/main/java/com/praadis/teacherapp/security/WebSecurityConfiguration.java
+++ src/main/java/com/praadis/teacherapp/security/WebSecurityConfiguration.java
@@ -1,11 +1,19 @@
 package com.praadis.teacherapp.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 // After Extending Spring will not Configure the method override here
 @Configuration
@@ -20,4 +28,21 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()))
                 .httpBasic();
     }
+
+    @Autowired
+    private DataSource dataSource ;
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        DelegatingPasswordEncoder encoder = (DelegatingPasswordEncoder) PasswordEncoderFactories.createDelegatingPasswordEncoder() ;
+        return  encoder ;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource).passwordEncoder(getPasswordEncoder());
+    }
+
+
 }
